19d4e8ece59a39cdd4b4720245f96cebeb32bf0d
Concours99 <geiserw@gmail.com>
2019-12-19 16:36:14 -0500

    Changes lost in last merge

--- wg_helper.py
+++ wg_helper.py
@@ -1,3 +1,4 @@
+""" Helper routines I find useful for most of my python code"""
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # Copyright (C) 2018, Wayne Geiser (geiserw@gmail.com).  All Rights Reserved
@@ -6,40 +7,37 @@
 #
 # All functions, variables, etc. should start with "WG" so as not to interfere with other
 # packages.
-
-WGHelper_version = "1.0"
-
 import datetime
+import pprint
+
+WG_HELPER_VERSION = "2.01"
 
 ###############################################################################
 #
 # Print out a trace message and flush the buffers.
 #
-def wg_trace_print(message, TRACE):
-    if TRACE:
+def wg_trace_print(message, trace):
+    """ Print out a tracing message."""
+    if trace:
         today = datetime.datetime.now()
-        outstring = (today.strftime("%x %X")
-                     + " - "
-                     + message)
+        outstring = (today.strftime("%x %X") + " - " + message)
         print(outstring, flush=True)
 
 ###############################################################################
 #
 # Print out an error message and flush the buffers.
 #
-def wg_error_print(where, message) :
+def wg_error_print(where, message):
+    """Print out an error message."""
     outstring = "Error in " + where + "! " + message
     wg_trace_print(outstring, True)
 
 ###############################################################################
 #
-# Print out a trace message and flush the buffers.
+# Print out a structure if we're tracing
 #
-def wg_trace_pprint(message, TRACE):
-    if TRACE:
-        today = datetime.datetime.now()
-        outstring = (today.strftime("%x %X")
-                     + " - "
-                     + message)
-        print(outstring, flush=True)
-        
+def wg_trace_pprint(struct, trace):
+    """Nicely print out a structure if we're tracing"""
+    if trace:
+        pprt = pprint.PrettyPrinter(indent=4)
+        pprt.pprint(struct)
